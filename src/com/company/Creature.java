d506daaf4e5abac322725a09879f938b1525e2bf
Orion Culbertson <orionyec@gmail.com>
2019-10-21 11:26:12 -0700

    First Commit

--- src/com/company/Creature.java
+++ src/com/company/Creature.java
@@ -1,5 +1,52 @@
 package com.company;
 
 public class Creature {
+    private int numberOfEyes;
+    private int numberOfArms;
+    private int numberOfLegs;
+//    private numberOfTails;
+//    private numberOfAntenna;
+
+    //Default Creature Constructor
+    public Creature(){
+        this.numberOfEyes = 0;
+        this.numberOfArms = 0;
+        this.numberOfLegs = 0;
+    }
+
+    //Creature Constructor with parameters
+    public Creature(int numOfEyes, int numOfArms, int numOfLegs){
+        this.numberOfEyes = numOfEyes;
+        this.numberOfArms = numOfArms;
+        this.numberOfLegs = numOfLegs;
+    }
+
+    public void printCreatureStats(){
+        System.out.println("Your creature has " + this.numberOfEyes + " eye(s), " + this.numberOfArms + " arm(s), and " + this.numberOfLegs + " leg(s).");
+    }
+
+    public int getNumberOfEyes() {
+        return this.numberOfEyes;
+    }
+
+    public void setNumberOfEyes(int numberOfEyes) {
+        this.numberOfEyes = numberOfEyes;
+    }
+
+    public int getNumberOfArms() {
+        return this.numberOfArms;
+    }
+
+    public void setNumberOfArms(int numberOfArms) {
+        this.numberOfArms = numberOfArms;
+    }
+
+    public int getNumberOfLegs() {
+        return this.numberOfLegs;
+    }
+
+    public void setNumberOfLegs(int numberOfLegs) {
+        this.numberOfLegs = numberOfLegs;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("*******************************************************");
+        System.out.println("*****************CREATURE CREATOR 4.20*****************");
+        System.out.println("*******************************************************\n");
+
+        System.out.print("Enter the number of eyes your creature will have: ");
+        int numberOfEyes = input.nextInt();
+
+        System.out.print("Enter the number of arms your creature will have: ");
+        int numberOfArms = input.nextInt();
+
+        System.out.print("Enter the number of legs your creature will have: ");
+        int numberOfLegs = input.nextInt();
+
+        System.out.println("\nGENERATING CREATURE.....\n");
+
+        Creature creatureObject = new Creature(numberOfEyes, numberOfArms, numberOfLegs);
+
+        creatureObject.printCreatureStats();
+
     }
 }
